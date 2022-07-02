1e274dd2e31404368d95e2152ae38b6068ef782f
Sergey Babich <88515146+KreoManser@users.noreply.github.com>
2022-07-03 00:16:49 +0300

    Finished Login View

--- ListOfContacts/LoginViewController.swift
+++ ListOfContacts/LoginViewController.swift
@@ -20,26 +20,31 @@ class LoginViewController: UIViewController {
     }
     
     @IBAction func nextButtonDidTap(_ sender: Any) {
+        let phoneLength = 11
         let numPhone = loginLabel.text ?? ""
         let countSymbolsInLine = numPhone.count
         let PersonalInfo = (countSymbolsInLine, numPhone)
         
-        guard let wrongVC = storyboard?.instantiateViewController(withIdentifier: "WrongViewController") else { return }
-        guard let contactListVC = storyboard?.instantiateViewController(withIdentifier: "ContactListTableViewController") else { return }
+        guard let wrongVC = storyboard?.instantiateViewController(withIdentifier: "WrongViewController") as? WrongViewController else { return }
+        
+        guard let contactListVC = storyboard?.instantiateViewController(withIdentifier: "ContactListTableViewController") as? ContactListTableViewController else { return }
         
         
         switch PersonalInfo {
         case (12..., _):
-            print("Неверный формат!")
+//            print("Неверный формат!")
+            wrongVC.error = "Cократите номер на \(countSymbolsInLine - phoneLength) символов"
             present(wrongVC, animated: true)
         case (..<11, _):
-            print("Неверный формат!")
+//            print("Неверный формат!")
+            wrongVC.error = "Дополните номер на \(phoneLength - countSymbolsInLine) символов"
             present(wrongVC, animated: true)
         case (11, _) where numPhone.hasPrefix("89087"):
-            print("Авторизация успешна!")
+//            print("Авторизация успешна!")
             navigationController?.pushViewController(contactListVC, animated: true)
         default:
-            print("Неверный формат!")
+//            print("Неверный формат!")
+            wrongVC.error = "Неверный формат!"
             present(wrongVC, animated: true)
         }
     }
